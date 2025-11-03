18d0cb794e34c6a51b0bc3b3a2e0f140eb181452
giuliagranado <giulia.granado@gmail.com>
2025-11-03 12:44:04 -0300

    Create A.py

---
+++ Internas_Maratona/Internas2017/A.py
@@ -0,0 +1,10 @@
+"""
+ pós-fixo - esquerda, direita, raiz
+ percurso infixo - esquerda, raiz, direita
+percurso prefixo - raiz, esquerda, direita
+"""
+#incompleto - só 
+#comecei 
+
+def prefixo(raiz, esquerda, direita):
+    entrada = list(input())
