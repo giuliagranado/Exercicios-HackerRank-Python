7e82cbe1273c6c22eac092ef91828785e79c82b2
giuliagranado <giulia.granado@gmail.com>
2025-11-12 20:39:05 -0300

    Update notas.py

--- Internas_Maratona/Internas 2025/original/notas.py
+++ Internas_Maratona/Internas 2025/original/notas.py
@@ -4,12 +4,15 @@ Created on Sat Nov  1 14:21:02 2025
 
 @author: lab54
 """
+n = int(input())
 
-string = input()
-n = 0
-for i in string:
-    if i == "A":
-        n += 1
-    if i == "a":
-        n += 1
-print(n)
+todas_notas = []
+
+for i in range(n):
+    num_notas = int(input())
+    notas = input().split()
+    for j in range(num_notas):
+        todas_notas.append(notas[j])
+
+ordenados = sorted(todas_notas, reverse=True)
+print(ordenados)
